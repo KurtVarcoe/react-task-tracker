fa28c8bf87031cfed81c3a1202660c2039cb7d10
Kurt <kurtvarcoe@gmail.com>
2021-10-12 22:12:00 +0200

    added green reminder border

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,6 @@
 const Task = ({task}) => {
     return (
-        <div className='task'>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`}>
             <h3>{task.text}</h3>
             <p>{task.day}</p>
         </div>
